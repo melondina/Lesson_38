978af7400c803b2667e3f0391c31764c772b4ee6
Dina Migunova <migunova.dina@gmail.com>
2023-03-15 10:50:43 +0100

    Add setter and getter

--- src/pet.java
+++ src/pet.java
@@ -11,5 +11,38 @@ public class pet {
     String birth;
     double weight;
 
+    public pet(Kind kind, String name, String birth, double weight) {
+        this.kind = kind;
+        this.name = name;
+        this.birth = birth;
+        this.weight = weight;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Kind getKind() {
+        return kind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
